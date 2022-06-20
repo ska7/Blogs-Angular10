bac0bd80b11e16b2126538344657f0e9a3260a61
Nick Aralin <nikkofann@gmail.com>
2022-06-20 17:41:16 -0400

    refactor: styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { PostInputComponent } from './post-input/post-input.component';
 import { PostsComponent } from './posts/posts.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostInputComponent,
     PostsComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
